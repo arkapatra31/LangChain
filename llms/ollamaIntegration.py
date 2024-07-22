11dd139ed73be9b8f8ca9d0650efbbb18c2e83d8
Arka Patra <arkapatra2009@gmail.com>
2024-07-22 19:35:18 +0530

    Ollama Integration

---
+++ llms/ollamaIntegration.py
@@ -0,0 +1,18 @@
+from langchain.prompts import ChatPromptTemplate
+from langchain_community.llms.ollama import Ollama
+
+template = """
+    Tell me something about {question} and return me a summary in a JSON body containing QN and ANS key
+"""
+
+prompt = ChatPromptTemplate.from_template(template)
+
+llm = Ollama(
+    base_url='http://localhost:11434',
+    model="llama3:latest"
+)
+
+chain = prompt | llm
+
+answer = chain.invoke({"question":"facebook"})
+print(answer)
