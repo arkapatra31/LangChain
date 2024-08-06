d3c00c290b432a878169941fea464b3e68e8e398
Arka Patra <arkapatra2009@gmail.com>
2024-08-06 23:31:51 +0530

    Code Cleanup

---
+++ RAG/extractor.py
@@ -0,0 +1,12 @@
+from langchain_community.document_loaders import PyPDFLoader
+
+
+def extract_document(filePath):
+    file = PyPDFLoader(file_path=filePath, extract_images=False)
+    documents = file.load_and_split()
+    return documents
+
+
+__all__ = [
+    extract_document
+]
--- RAG/qna.py
+++ RAG/qna.py
@@ -1,68 +1,47 @@
 import os
-
-from PyPDF2 import PdfReader
 from dotenv import load_dotenv
 from langchain_groq import ChatGroq
-from langchain_core.prompts import ChatPromptTemplate
 from langchain.chains.question_answering import load_qa_chain
-from langchain_community.vectorstores import FAISS, InMemoryVectorStore
-from langchain_community.llms import Ollama
 from langchain_community.embeddings import OllamaEmbeddings
-from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_pinecone import PineconeVectorStore
+from RAG.extractor import extract_document
 
 load_dotenv()
 apiSecret = os.getenv("GROQ_API_SECRET")
-question = "Monitoring"
+pinecone_api_key = os.getenv("PINECONE_API_KEY")
 
-text = ""
-file = open('../data/2022-commercetools-resiliency.pdf', 'rb+')
-if file is not None:
-    print("File exists")
-    pdfReader = PdfReader(file)
+# Initialise Pinecone VDB
+index_name = "ct-docs"
 
-    for x in range(len(pdfReader.pages)):
-        page = pdfReader.pages[x]
-        text += page.extract_text()
-    if text is not None:
-        print("Text Extracted")
+# Retrieve Documents / Chunks
+chunks = extract_document(filePath="../data/2022-commercetools-resiliency.pdf")
 
-    text_splitter = RecursiveCharacterTextSplitter(
-        length_function=len,
-        separators=["\n"],
-        chunk_size=150,
-        chunk_overlap=100
-    )
+# Create embeddings
+embeddings = OllamaEmbeddings(model="llama3")
 
-    # Split the entire content into chunks
-    chunks = text_splitter.split_text(text)
+# Initialise LLM
+llm = ChatGroq(temperature=0.1, model="llama3-70b-8192", api_key=apiSecret)
 
-    if chunks is not None:
-        print(f''' Text Splitted into {len(chunks)} chunks''')
+# Create the vector store
+#vector_store = PineconeVectorStore.from_documents(documents=chunks, index_name=index_name, embedding=embeddings)
+vector_store = PineconeVectorStore(index_name=index_name, embedding=embeddings)
+if vector_store:
+    print("Vector Store Created")
 
-    # Create embeddings
-    embeddings = OllamaEmbeddings(model="llama3")
 
-    # Initialise LLM
-    # llm = ChatGroq(temperature=0.1, model="llama3-70b-8192", api_key=apiSecret)
-    llm = Ollama(
-        base_url='http://localhost:11434',
-        model="llama3:latest"
-    )
+def generate_response(question):
+    try:
+        if question:
+            print(f"""Question is here :- {question}""")
+            match = vector_store.similarity_search(question)
+            chain = load_qa_chain(llm, chain_type="stuff", verbose=True)
+            response = chain.invoke(input={"input_documents": match, "question": question})
+            print(response)
+            return response
+    except Exception as ex:
+        print(ex)
 
-    print("Embedding and LLM Initialised")
 
-    # Create the vector store
-    vector_store = FAISS.from_texts(chunks, embeddings)
-
-    if vector_store:
-        print("Vector Store Created")
-        print(vector_store)
-
-    if question:
-        print(f"""Question is here :- {question}""")
-        match = vector_store.similarity_search(question)
-        print(match)
-
-        chain = load_qa_chain(llm, chain_type="stuff", verbose=True)
-        response = chain.invoke(input_data=match, question=embeddings.embed_query(question))
-        print(response)
+__all__ = [
+    generate_response
+]
