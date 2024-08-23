4657a7dfc980f91051cdf8563418d25c3f31e57e
Arka Patra <arkapatra2009@gmail.com>
2024-08-24 03:01:35 +0530

    React agent (#8)
    * Handling AgentFinish instance
    
    * Handling AgentFinish instance

--- ReAct-Agent/main.py
+++ ReAct-Agent/main.py
@@ -1,16 +1,18 @@
+import json
 from typing import Union, List
 
 from dotenv import load_dotenv
 from langchain.agents import tool
+from langchain.agents.format_scratchpad import format_log_to_str
 from langchain.prompts import PromptTemplate
 from langchain.tools.render import render_text_description
 from langchain.agents.output_parsers import ReActSingleInputOutputParser
 from cloudLLM.GroqLLM import groq_llm
 from langchain_core.agents import AgentAction, AgentFinish
 from langchain_core.tools import Tool
+from callbacks import AgentCallbackHandler
 
 load_dotenv()
-
 
 @tool()
 def get_text_length(text: str):
@@ -18,11 +20,13 @@ def get_text_length(text: str):
     text = text.strip("\n").strip('"')
     return len(text)
 
+
 def find_tool_by_name(tools: List[Tool], tool_name: str) -> Tool:
     for tool in tools:
         if tool.name == tool_name:
             return tool
     raise ValueError(f"""Could not find tool with name {tool_name}""")
+
 
 if __name__ == "__main__":
     tools = [get_text_length]
@@ -46,7 +50,7 @@ if __name__ == "__main__":
     Begin!
     
     Question: {input}
-    Thought:
+    Thought: {agent_scratchpad}
     """
 
     # Use render_text_description on tools object as LLMs only accepts string as input
@@ -54,18 +58,35 @@ if __name__ == "__main__":
         tools=render_text_description(tools), tool_names=",".join(t.name for t in tools)
     )
 
-    #Assigning stop to stop sequences on finding the particular value
+    # Assigning stop to stop sequences on finding the particular value
     groq_llm.stop = ["\nObservation"]
+    groq_llm.callbacks=[AgentCallbackHandler()]
     llm = groq_llm
-    agent = {"input": lambda x: x["input"]} | prompt_template | llm | ReActSingleInputOutputParser()
+    intermediate_steps = []
+    agent = {"input": lambda x: x["input"], "agent_scratchpad": lambda x: format_log_to_str(
+        x["agent_scratchpad"])} | prompt_template | llm | ReActSingleInputOutputParser()
 
-    agent_step: Union[AgentAction, AgentFinish] = agent.invoke({"input": "What is the length of 'DOG' in characters"})
-    print(f"""Agent step ----> \n {agent_step}""")
+    agent_step = ""
+    while not isinstance(agent_step, AgentFinish):
+        agent_step: Union[AgentAction, AgentFinish] = agent.invoke(
+            {"input": "What is the length of 'DOG' in characters", "agent_scratchpad": intermediate_steps})
 
-    if isinstance(agent_step, AgentAction):
-        tool_name = agent_step.tool
-        tool_to_use = find_tool_by_name(tools, tool_name)
-        tool_input = agent_step.tool_input.strip("\n").strip("'")
+        # Agent
+        # step - --->
+        # tool = 'get_text_length'
+        # tool_input = "'DOG'"
+        # log = "Let's get started.\n\nThought: I need to find the length of the text 'DOG' in characters.\n\nAction: get_text_length\nAction Input: 'DOG'"
+        # print(f"""Agent step ----> \n {agent_step}""")
 
-        observation = tool_to_use.func(str(tool_input))
-        print(f"""{observation=}""")
+        if isinstance(agent_step, AgentAction):
+            tool_name = agent_step.tool
+            tool_to_use: Tool = find_tool_by_name(tools, tool_name)
+            tool_input = agent_step.tool_input.strip("\n").strip("'")
+
+            observation = tool_to_use.func(str(tool_input))
+            print(f"This is Agent Action Instance. Details : {agent_step}\n")
+            print(f"""{observation=}\n""")
+            intermediate_steps.append((agent_step, str(observation)))
+
+    if isinstance(agent_step, AgentFinish):
+        print(f"This is Agent Finish Instance. Details : {agent_step}")
