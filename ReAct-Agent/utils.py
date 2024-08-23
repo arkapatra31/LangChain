403c1477f4dc788ff5c3ffed87e11fd18cd1d083
Arka Patra <arkapatra2009@gmail.com>
2024-08-24 01:48:15 +0530

    Handling AgentFinish instance

---
+++ ReAct-Agent/utils.py
@@ -0,0 +1,17 @@
+from typing import Tuple, List
+
+from langchain_core.agents import AgentAction
+
+
+def format_log_to_string(
+        intermediate_steps: List[Tuple[AgentAction, str]],
+        observation_prefix: str = "Observation",
+        llm_prefix: str = "Thought :"
+) -> str:
+    """ Construct the scratchpad that lets the agent continue its thought process """
+    thoughts = ""
+    for action, observation in intermediate_steps:
+        thoughts += action.log
+        thoughts += f"\n{observation_prefix}{observation}\n{llm_prefix}"
+    return thoughts
+
