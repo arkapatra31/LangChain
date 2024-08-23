b106f50e5a9810cfc5c2372921aae29d8068d709
Arka Patra <arkapatra2009@gmail.com>
2024-08-24 02:54:16 +0530

    Handling AgentFinish instance

--- ReAct-Agent/main.py
+++ ReAct-Agent/main.py
@@ -10,9 +10,9 @@ from langchain.agents.output_parsers import ReActSingleInputOutputParser
 from cloudLLM.GroqLLM import groq_llm
 from langchain_core.agents import AgentAction, AgentFinish
 from langchain_core.tools import Tool
+from callbacks import AgentCallbackHandler
 
 load_dotenv()
-
 
 @tool()
 def get_text_length(text: str):
@@ -60,33 +60,33 @@ if __name__ == "__main__":
 
     # Assigning stop to stop sequences on finding the particular value
     groq_llm.stop = ["\nObservation"]
+    groq_llm.callbacks=[AgentCallbackHandler()]
     llm = groq_llm
     intermediate_steps = []
     agent = {"input": lambda x: x["input"], "agent_scratchpad": lambda x: format_log_to_str(
         x["agent_scratchpad"])} | prompt_template | llm | ReActSingleInputOutputParser()
 
-    agent_step: Union[AgentAction, AgentFinish] = agent.invoke(
-        {"input": "What is the length of 'DOG' in characters", "agent_scratchpad": intermediate_steps})
+    agent_step = ""
+    while not isinstance(agent_step, AgentFinish):
+        agent_step: Union[AgentAction, AgentFinish] = agent.invoke(
+            {"input": "What is the length of 'DOG' in characters", "agent_scratchpad": intermediate_steps})
 
-    # Agent
-    # step - --->
-    # tool = 'get_text_length'
-    # tool_input = "'DOG'"
-    # log = "Let's get started.\n\nThought: I need to find the length of the text 'DOG' in characters.\n\nAction: get_text_length\nAction Input: 'DOG'"
-    # print(f"""Agent step ----> \n {agent_step}""")
+        # Agent
+        # step - --->
+        # tool = 'get_text_length'
+        # tool_input = "'DOG'"
+        # log = "Let's get started.\n\nThought: I need to find the length of the text 'DOG' in characters.\n\nAction: get_text_length\nAction Input: 'DOG'"
+        # print(f"""Agent step ----> \n {agent_step}""")
 
-    if isinstance(agent_step, AgentAction):
-        tool_name = agent_step.tool
-        tool_to_use: Tool = find_tool_by_name(tools, tool_name)
-        tool_input = agent_step.tool_input.strip("\n").strip("'")
+        if isinstance(agent_step, AgentAction):
+            tool_name = agent_step.tool
+            tool_to_use: Tool = find_tool_by_name(tools, tool_name)
+            tool_input = agent_step.tool_input.strip("\n").strip("'")
 
-        observation = tool_to_use.func(str(tool_input))
-        print(f"This is Agent Action Instance. Details : {agent_step}\n")
-        print(f"""{observation=}\n""")
-        intermediate_steps.append((agent_step, str(observation)))
-
-    agent_step: Union[AgentAction, AgentFinish] = agent.invoke(
-        {"input": "What is the length of 'DOG' in characters", "agent_scratchpad": intermediate_steps})
+            observation = tool_to_use.func(str(tool_input))
+            print(f"This is Agent Action Instance. Details : {agent_step}\n")
+            print(f"""{observation=}\n""")
+            intermediate_steps.append((agent_step, str(observation)))
 
     if isinstance(agent_step, AgentFinish):
         print(f"This is Agent Finish Instance. Details : {agent_step}")
