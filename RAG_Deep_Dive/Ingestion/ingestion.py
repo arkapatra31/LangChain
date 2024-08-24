e207b4bef220f809a652b64b4e011eff9b3967c8
Arka Patra <arkapatra2009@gmail.com>
2024-08-25 03:55:31 +0530

    ➡️FAISS Vector Store ➡️Ingestion to targeted vector store ➡️Retrieval from targeted vector store ➡️Saving a copy of FAISS index locally ➡️Load the local copy of FAISS index to use as a retriever

--- RAG_Deep_Dive/Ingestion/ingestion.py
+++ RAG_Deep_Dive/Ingestion/ingestion.py
@@ -1,19 +1,29 @@
 import os
+from builtins import int
+from typing import Any
 import streamlit as st
 from dotenv import load_dotenv
 from cloudLLM import groq_llm
 from langchain_community.embeddings import OllamaEmbeddings
 from langchain_pinecone import PineconeVectorStore
+from langchain_community.vectorstores import FAISS
 from RAG_Deep_Dive.utils.chunkifyData import convert_text_to_chunks
 from RAG_Deep_Dive.utils.pdfToText import extract_text_from_pdf
 
 load_dotenv()
 pinecone_api_key = os.getenv('PINECONE_API_KEY')
 pinecone_index = os.getenv('PINECONE_INDEX_NAME')
-
+faiss_local_index = os.getenv("FAISS_LOCAL_INDEX_CREATION")
 
 if __name__ == "__main__":
     # Use Streamlit to create a minimalistic UI for the file uploading
+    # Select Target Vector Store from User
+    target_vector_store = st.text_input("""
+    Choose Target Vector Store from the below options :\n
+    1. Pinecone
+    2. FAISS
+    """)
+
     st.title("Upload Documents to be embedded")
     uploaded_file = st.file_uploader("Upload a PDF file", type="pdf")
 
@@ -30,8 +40,20 @@ if __name__ == "__main__":
         # Instantiate LLM
         llm = groq_llm
 
-        # Initialise vector DB
-        vector_store = PineconeVectorStore.from_texts(texts=chunks, index_name=pinecone_index, embedding=embeddings)
+        vector_store: Any
+        if target_vector_store == "1":
+            # Initialise and load embedded documents to Pinecone Vector Store
+            vector_store: PineconeVectorStore = PineconeVectorStore.from_texts(
+                texts=chunks, index_name=pinecone_index,
+                embedding=embeddings)
+        elif target_vector_store == "2":
+            # Initialise and load embedded documents to FAISS Local Vector Store
+            vector_store = FAISS.from_texts(texts=chunks, embedding=embeddings)
+            vector_store.save_local(faiss_local_index)
+        else:
+            raise ValueError("Invalid Selection")
 
-        if vector_store:
-            print("Vector Store Created")
+        if isinstance(vector_store, PineconeVectorStore):
+            st.write("Pinecone Vector Store Created")
+        else:
+            st.write("FAISS Local Vector Store Created")
--- RAG_Deep_Dive/Retriever/retriever.py
+++ RAG_Deep_Dive/Retriever/retriever.py
@@ -1,16 +1,37 @@
 import streamlit as st
+from RAG_Deep_Dive.Local_Vector_Store.faiss_vector_store import faiss_retrieval_chain
 from RAG_Deep_Dive.Retriever.createRetrieverChain import retrieval_chain
 from RAG_Deep_Dive.Retriever.customPrompt import rag_chain
 
 # Input widget for user question
+st.header("Q&A")
+user_choice = st.text_input(
+    """
+    Which of the following you wish to use?\n
+    1.Pinecone Retriever with LangSmith provided QA Chat Prompt\n
+    2. Pinecone Retriever with Custom Prompt\n
+    3. FAISS as Retriever
+    """
+)
+
 user_question = st.text_input("Enter your question here")
 if user_question:
-    # Invoke Retrieval Chain
-    result = retrieval_chain.invoke(input={"input": user_question})
-    st.subheader("Response from Retrieval Chain")
-    st.write(result)
+    st.subheader("*****RESPONSE*****")
+    if user_choice == "1":
+        # Invoke Retrieval Chain using Pinecone as Vector Store with QA prompt from LangChain Hub
+        result = retrieval_chain.invoke(input={"input": user_question})
+        st.write(result)
 
-    res = rag_chain.invoke(user_question)
-    st.subheader("Response from RAG Chain with Custom Prompt")
-    st.write(res)
+    elif user_choice == "2":
+        # Invoke Retrieval Chain using Pinecone as Vector Store with custom prompt
+        result = rag_chain.invoke(user_question)
+        st.write(result)
 
+    elif user_choice == "3":
+        # Invoke Retrieval Chain using FAISS Local Vector Store
+        result = faiss_retrieval_chain.invoke({"input": user_question})
+        st.write(result)
+
+    else:
+        st.write("INVALID OPTION SELECTION")
+        print("Invalid Choice")
