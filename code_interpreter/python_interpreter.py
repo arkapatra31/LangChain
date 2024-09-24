a83eb277397bac92768390ea22f1c454db0b48e8
Arka Patra <arkapatra2009@gmail.com>
2024-09-24 23:48:51 +0530

    ➡️Understanding of Langchain Experimental Tools PKG ➡️Using the PythonREPL tool from Langchain ➡️Using the tool to write Python code ➡️Saving the reponse generated from the code

---
+++ code_interpreter/python_interpreter.py
@@ -0,0 +1,44 @@
+from dotenv import load_dotenv
+from langchain import hub
+from langchain_openai import ChatOpenAI
+from langchain.agents import create_react_agent, AgentExecutor
+from langchain_experimental.tools import PythonREPLTool
+
+load_dotenv()
+
+
+def main():
+    instructions = """You are an agent designed to write and execute python code to answer questions.
+    You have access to a python REPL, which you can use to execute python code.
+    If you get an error, debug your code and try again.
+    Only use the output of your code to answer the question. 
+    You might know the answer without running any code, but you should still run the code to get the answer.
+    If it does not seem like you can write code to answer the question, just return "I don't know" as the answer.
+    """
+
+    base_prompt = hub.pull("langchain-ai/react-agent-template")
+    prompt = base_prompt.partial(instructions=instructions)
+
+    tools = [PythonREPLTool()]
+
+    agent = create_react_agent(
+        llm=ChatOpenAI(temperature=0, model="gpt-4o-mini"),
+        tools=tools,
+        prompt=prompt
+    )
+
+    agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
+
+    agent_executor.invoke(
+        input={
+            "input": """Generate and save inside a folder named generated-qr-codes in the current working directory 2 QRcodes
+                                that point to these two URLs
+                                https://www.linkedin.com/in/arkapatra31/,
+                                https://www.facebook.com/31AKP,
+                                also name the qrcodes file based on the hostname of the URLS and you have qrcode package installed already."""
+        }
+    )
+
+
+if __name__ == '__main__':
+    main()
