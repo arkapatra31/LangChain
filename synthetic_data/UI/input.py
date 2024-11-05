0be761e9e476e82dc4e5f820034cc5bbb9d8a5a8
Arka Patra <arkapatra2009@gmail.com>
2024-11-05 19:52:16 +0530

    Ammending example values to each configuration for references

--- synthetic_data/UI/input.py
+++ synthetic_data/UI/input.py
@@ -14,12 +14,14 @@ if 'columns_to_remove' not in st.session_state:
     st.session_state.columns_to_remove = []
 if 'data_type' not in st.session_state:
     st.session_state.data_type = {}
+if 'example_data' not in st.session_state:
+    st.session_state.example_data = {}
 if 'df' not in st.session_state:
     st.session_state.df = pd.DataFrame()  # Initialize empty DataFrame
 
 # Input fields
 domain = st.text_input("Enter domain of data you are looking for")
-number_of = st.number_input("Enter the number of columns:", min_value=1, step=1)
+number_of = st.number_input("Enter the number of columns:", min_value=1, max_value=15, step=1)
 
 # Button to generate columns
 if st.button("Generate Columns"):
@@ -32,10 +34,10 @@ if st.button("Generate Columns"):
 # Show the generated DataFrame
 if st.session_state.df.empty:
     for column in st.session_state.df.columns:
-        col1, col2, col3 = st.columns([1, 1, 1])
+        col1, col2, col3, col4 = st.columns([1, 1, 1, 2.5])
 
         # Show column name
-        col1.write(f"**{column}**")
+        col1.write(column)
 
         if column not in st.session_state.data_type:
             # Set default data type for the column
@@ -43,7 +45,7 @@ if st.session_state.df.empty:
 
         # Selectbox to choose data type for the column
         col_type = col2.selectbox(
-            f"Select data type for {column}",
+            "Data Type",
             ["string", "int"],
             key=f"type_{column}",
             index=["string", "int"].index(st.session_state.data_type[column])
@@ -62,6 +64,11 @@ if st.session_state.df.empty:
             if column in st.session_state.columns_to_remove:
                 st.session_state.columns_to_remove.remove(column)
 
+        # Show the 4th column to accept example data
+        example_data = col4.text_input("Example data", key=f"example_{column}",
+                                       placeholder="Enter comma delimited example values")
+        st.session_state.example_data[column] = example_data
+
     # Button to apply modifications and update the DataFrame
     if st.button("Apply Modifications"):
         # Drop the selected columns
@@ -72,15 +79,21 @@ if st.session_state.df.empty:
         st.dataframe(st.session_state.df)
         # if not os.path.exists("../data"):
         #     os.mkdir("../data")
-        #st.session_state.df.to_csv(f"""../data/{domain}.csv""", index=False, mode='w')
+        # st.session_state.df.to_csv(f"""../data/{domain}.csv""", index=False, mode='w')
         configuration = st.session_state.data_type
         final_columns = st.session_state.df.columns.tolist()
+        example_data = st.session_state.example_data
         if not os.path.exists("../data"):
             os.mkdir("../data")
-        with open(f"../data/{domain}.json", "w") as f:
-            f.write("{")
+        # Save data type and example_data against the columns in a JSON format to a single configuration to a file
+        with open(f"../data/{domain}_config.json", "w") as f:
+            f.write('{')
             for column in final_columns:
-                f.write(f'"{column}": "{configuration[column]}",')
-            f.write("}")
+                #Split the example data by comma and strip whitespaces
+                examples = [example.strip() for example in example_data[column].split(",") if example] if example_data[column] else []
+                f.write(f'"{column}": {{"data_type": "{configuration[column]}", "example_data": "{examples}"}}')
+                if column != final_columns[-1]:
+                    f.write(", ")
+            f.write('}')
         f.close()
-
+        st.success(f"Data saved to ../data/{domain}_config.json")
