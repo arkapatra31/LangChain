4d8478faa3b2d928578474f218c331550124ef72
Arka Patra <arkapatra2009@gmail.com>
2024-11-25 21:46:43 +0530

    ➡️Add validation to customer name based on gender ➡️Restructuring the hierarchy ➡️Updating the generated data

--- synthetic_data/__init__.py
+++ synthetic_data/__init__.py
@@ -3,8 +3,9 @@ from synthetic_data.services.product.generate_and_modify import get_dataframe_co
 from synthetic_data.services.product.generate_dataset import read_dataframe_and_generate_data
 from synthetic_data.services.customer.generate_Address_for_EU import generate_address_for_EU
 from synthetic_data.services.customer.generate_Address_for_US import generate_address_for_US
+from synthetic_data.services.customer.generate_customer_data import generate_customers_data
 from synthetic_data.services.orders.generate_order import generate_orders
 from synthetic_data.services.order_line_items.generate_order_line_items import generate_order_line_items_details
 
 __all__ = [llm, get_dataframe_columns, read_dataframe_and_generate_data, generate_address_for_EU,
-               generate_address_for_US, generate_orders, generate_order_line_items_details]
+           generate_address_for_US, generate_customers_data, generate_orders, generate_order_line_items_details]
