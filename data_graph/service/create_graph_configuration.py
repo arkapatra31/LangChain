ee959b321a0ea4358fb84af1a73e225e4b9e980a
Arka Patra <arkapatra2009@gmail.com>
2025-01-26 15:02:22 +0530

    Refactor prompt template for JSON configuration in data analysis

--- data_graph/service/create_graph_configuration.py
+++ data_graph/service/create_graph_configuration.py
@@ -3,47 +3,47 @@ from llms import gemini_llm
 from langchain.prompts import PromptTemplate
 
 def fetchPromptForJSONConfiguration():
-    # template = """
-    #     DEFINE LLM ROLE:
-    #     - You are a Data Analyst and you are excellent in analyzing data and mapping them with 0 data loss
-    #
-    #     INPUT:
-    #     Here is your input data:
-    #     {input_data}
-    #
-    #     TASK DESCRIPTION:
-    #     You need to analyse the records and then return only the JSON content in JSON format. The JSON format details are as follows:
-    #     1. nodes: Dictionary of nodes with the following details:
-    #         - id: str = Unique identifier of the node
-    #         - label: str =  Label of the node
-    #         - properties: Optional[dict] = Properties of the node
-    #     2. relationships: List of relationships with the following details:
-    #         - sourceId: str = Unique identifier of the source node
-    #         - targetId: str = Unique identifier of the target node
-    #         - label: str = Label of the relationship
-    #         - properties: Optional[dict] = Properties of the relationship containing a foreign key and the value
-    #
-    #     IMPORTANT:
-    #     Make sure do not return any other information or metadata
-    # """
-
     template = """
-            DEFINE LLM ROLE:
-            - You are a Data Analyst and you are excellent in analyzing data and mapping them with 0 data loss
+        DEFINE LLM ROLE:
+        - You are a Data Analyst and you are excellent in analyzing data and mapping them with 0 data loss
 
-            INPUT:
-            Here is your input data:
-            {input_data}
+        INPUT:
+        Here is your input data:
+        {input_data}
 
-            TASK DESCRIPTION:
-            You need to analyse the records and then return only the JSON content in JSON format. The JSON format details are as follows:
-            1. nodes: Dictionary of nodes with all the details of the rows in the input data
-                
-            2. relationships: List of relationships with all the links between the nodes in the input data
+        TASK DESCRIPTION:
+        You need to analyse the records and then return only the JSON content in JSON format. The JSON format details are as follows:
+        1. nodes: Dictionary of nodes with the following details:
+            - id: str = Unique identifier of the node
+            - label: str =  Label of the node
+            - properties: Optional[dict] = Properties of the node
+        2. relationships: List of relationships with the following details:
+            - sourceId: str = Unique identifier of the source node
+            - targetId: str = Unique identifier of the target node
+            - label: str = Label of the relationship
+            - properties: Optional[dict] = Properties of the relationship containing a foreign key and the value
 
-            IMPORTANT:
-            Make sure do not return any other information or metadata
-        """
+        IMPORTANT:
+        Make sure do not return any other information or metadata
+    """
+
+    # template = """
+    #         DEFINE LLM ROLE:
+    #         - You are a Data Analyst and you are excellent in analyzing data and mapping them with 0 data loss
+    #
+    #         INPUT:
+    #         Here is your input data:
+    #         {input_data}
+    #
+    #         TASK DESCRIPTION:
+    #         You need to analyse the records and then return only the JSON content in JSON format. The JSON format details are as follows:
+    #         1. nodes: Dictionary of nodes with all the details of the rows in the input data
+    #
+    #         2. relationships: List of relationships with all the links between the nodes in the input data
+    #
+    #         IMPORTANT:
+    #         Make sure do not return any other information or metadata
+    #     """
 
     prompt_template = PromptTemplate(
         template=template,
